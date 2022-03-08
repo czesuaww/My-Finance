c5fa61ca8859bf685ffecd5e4826f42a7e15ae56
Grzegorz_Szwed <greg152295@gmail.com>
2022-03-08 10:29:58 +0100

    added function to check if all inputs are selected

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const expensesSection = document.querySelector(".expensesArea");
 const availableMoney = document.querySelector(".options__head-text");
 
 const addTransactionPanel = document.querySelector(".addTransactionPanel");
-const nameImput = document.querySelector("#name");
+const nameInput = document.querySelector("#name");
 const amountImput = document.querySelector("#amount");
 const categorySelect = document.querySelector("#category");
 
@@ -27,13 +27,31 @@ let moneyArray = [0];
 
 const showPanel = () => (addTransactionPanel.style.display = "flex");
 
-const hidePanel = () => (addTransactionPanel.style.display = "none");
+const hidePanel = () => {
+  addTransactionPanel.style.display = "none";
+  clearInputs();
+};
 
 const showChangeColor = () => (showColorPanel.style.visibility = "visible");
 
 const hideChangeColor = () => (showColorPanel.style.visibility = "hidden");
 
+const clearInputs = () => {
+  nameInput.value = "";
+  amountImput.value = "";
+  categorySelect.value = "none";
+};
+const checkForms = () => {
+  selectedCategory = categorySelect.options[categorySelect.selectedIndex].value;
+  if (nameInput.value !== "" && amountImput.value !== "" && selectedCategory !== "none") {
+    console.log("ok");
+  } else {
+    alert("Uzupełnij wszystkie pola!");
+  }
+};
+
 changeColor.addEventListener("click", showChangeColor);
 closeStylePanel.addEventListener("click", hideChangeColor);
 addTransactionBtn.addEventListener("click", showPanel);
 cancelBtn.addEventListener("click", hidePanel);
+saveBtn.addEventListener("click", checkForms);
